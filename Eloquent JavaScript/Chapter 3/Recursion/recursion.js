997201b3427009d53d9ccac54d9f5465de6dc10f
Lee Maurin <leem4421@gmail.com>
2019-03-12 13:38:25 -0500

    Add files via upload

---
+++ Eloquent JavaScript/Chapter 3/Recursion/recursion.js
@@ -0,0 +1,14 @@
+// A recursive function, isEven, accepts a single parameter (a positive, whole number) and returns a Boolean.
+
+function isEven(a) {
+  if (a % 2 == 0) {
+    console.log(`${a} is an even number (True).`);
+  } else {
+    console.log(`${a} is an odd number (False).`);
+  }
+}
+
+isEven(50);
+isEven(75);
+isEven(-1);
+isEven(-2);
