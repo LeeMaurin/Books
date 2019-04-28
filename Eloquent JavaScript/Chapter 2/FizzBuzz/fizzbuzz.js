0e5a66d1ecea538a8fcd7c2b79bc1863368e658b
Lee Maurin <leem4421@gmail.com>
2019-04-28 14:58:02 -0500

    Update fizzbuzz.js

--- Eloquent JavaScript/Chapter 2/FizzBuzz/fizzbuzz.js
+++ Eloquent JavaScript/Chapter 2/FizzBuzz/fizzbuzz.js
@@ -1,4 +1,6 @@
-// This is a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, the program prints "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), it prints "Buzz" instead.
+// This is a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+// For numbers divisible by 3, the program prints "Fizz" instead of the number. 
+// For numbers divisible by 5 (and not 3), it prints "Buzz" instead.
 // The program prints "FizzBuzz" for numbers that are divisible by both 3 and 5.
 
 for (var i = 1; i <= 100; i++) {
