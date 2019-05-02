877222b883ba2a75d46f4dc007137ac0b8ac1342
Lee Maurin <leem4421@gmail.com>
2019-05-02 13:28:19 -0500

    Update aSumOfRange.js

--- Eloquent JavaScript/Chapter 4/The Sum of a Range/aSumOfRange.js
+++ Eloquent JavaScript/Chapter 4/The Sum of a Range/aSumOfRange.js
@@ -24,7 +24,12 @@ function rangeSum(a, b) {
 
 rangeSum(1, 10);
 
-// Part 3 (optional) - As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+// Part 3 (optional) - As a bonus assignment, modify your range function to 
+// take an optional third argument that indicates the “step” value used when 
+// building the array. If no step is given, the elements go up by increments of one, 
+// corresponding to the old behavior. The function call range(1, 10, 2) should
+// return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so
+// that range(5, 2, -1) produces [5, 4, 3, 2].
 
 function rangeIncrementalOutput(a, b, c) {
   let rangeIO = [];
