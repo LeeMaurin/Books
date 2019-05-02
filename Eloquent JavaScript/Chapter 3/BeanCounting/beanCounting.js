9cdd7e08e1f7bc3ff3d89300cb4312c826617702
Lee Maurin <leem4421@gmail.com>
2019-05-01 19:29:04 -0500

    Add files via upload

---
+++ Eloquent JavaScript/Chapter 3/BeanCounting/beanCounting.js
@@ -0,0 +1,16 @@
+function countBs(a) {
+  console.log(countChar(a, 'B'));
+}
+
+function countChar(string, ca) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ca) {
+      count += 1;
+    }
+  }
+  console.log(count);
+}
+
+countBs('BBB');
+countChar('beancounting', 'n');
